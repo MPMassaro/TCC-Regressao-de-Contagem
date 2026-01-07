abecea1efd493fb2510d4e6fa8c5e7550dbf08f5
MPMassaro <murilomassaro03@gmail.com>
2026-01-07 18:28:03 -0300

    .

---
+++ Aplicações/Aplicação 1.R
@@ -0,0 +1,4 @@
+# Pacotes
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load("tidyverse","GLMsData","gamlss", "Cairo", "gamlss", "numDeriv")
---
+++ Aplicações/Aplicação 2.R
@@ -0,0 +1,1 @@
+if (!require("pacman")) install.packages("pacman")
